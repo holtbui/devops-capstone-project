135799ac1916fb35fe16e2705120515952eb07d6
holtbui <57018281+holtbui@users.noreply.github.com>
2023-04-24 22:16:12 -0700

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -146,14 +146,14 @@ class TestAccountService(TestCase):
         print(data)
         self.assertEqual(data["name"], account.name)
         
-     def test_account_not_found(self):
+    def test_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(
             f"{BASE_URL}/{0}"
         )
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
         
-     def test_update_account(self):
+    def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
         test_account = AccountFactory()
@@ -198,3 +198,4 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+
